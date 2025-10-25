61ef1d953b196de876741c8d65b620c250743960
Ryan Prado <ryanpradop@gmail.com>
2025-10-25 14:51:54 -0300

    chore: add semantic release configuration and update package.json
    - Added "type": "module" to package.json for ES module support.
    - Introduced "release" script in package.json to automate versioning and changelog generation using semantic release.
    - Added necessary devDependencies for semantic release, including changelog, git, and GitHub plugins.
    - Created release.config.js to configure semantic release with specified branches and plugins.

---
+++ release.config.js
@@ -0,0 +1,27 @@
+export default {
+  branches: ["main"],
+  repositoryUrl: "https://github.com/RyanPrado/eniac_projeto_2_sem.git",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: ["build/**"],
+      },
+    ],
+  ],
+};
