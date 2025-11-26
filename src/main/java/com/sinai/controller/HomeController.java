ca0d56b4a14b59385c9b05259cc53c64c3153d45
ymillerr <millerandvitor@gmail.com>
2025-11-26 11:47:45 +0000

    fix: removed database connection from HomeController

--- src/main/java/com/sinai/controller/HomeController.java
+++ src/main/java/com/sinai/controller/HomeController.java
@@ -1,19 +1,11 @@
 package com.sinai.controller;
 
-import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
-import java.sql.Connection;
-
-import java.sql.PreparedStatement;
-
-import javax.sql.DataSource;
 
 import com.sinai.service.CourseService;
 
@@ -24,35 +16,9 @@ import com.sinai.service.CourseService;
 public class HomeController extends HttpServlet {
 
     private final CourseService courseService = new CourseService();
-    private DataSource connectionDataSource;
-
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-
-        try {
-            ServletContext context = config.getServletContext();
-            this.connectionDataSource = (DataSource) context.getAttribute("DB_CONNECTION");
-        } catch (Exception e) {
-          System.out.println(e.getMessage());
-        }
-    }
-
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String sql = "INSERT INTO registros (nome, idade) VALUES (?, ?)";
-        try (Connection connection = connectionDataSource.getConnection()) {        
-
-            PreparedStatement sqlQuery = connection.prepareStatement(sql);
-            sqlQuery.setString(1, "Cleiton");
-            sqlQuery.setInt(2, 25);
-            
-            sqlQuery.executeUpdate();
-            } catch (Exception e) {
-                e.getStackTrace();
-                System.out.println("Erro ao executar a query: " + e.getMessage());
-            }
-
         req.setAttribute("courses", courseService.findFeaturedCourses());
         req.getRequestDispatcher("/WEB-INF/views/pages/home.jsp").forward(req, resp);
     }
