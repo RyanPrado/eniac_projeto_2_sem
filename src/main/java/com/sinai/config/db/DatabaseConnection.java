2564c4e18144734ceeec1d7f523c8bf8d63a012a
ymillerr <millerandvitor@gmail.com>
2025-11-26 11:36:25 +0000

    fix: add new layer of envs verification

--- src/main/java/com/sinai/config/db/DatabaseConnection.java
+++ src/main/java/com/sinai/config/db/DatabaseConnection.java
@@ -6,14 +6,28 @@ import com.zaxxer.hikari.HikariDataSource;
 
 public class DatabaseConnection {
   private DataSource connectionDataSource;
+  private final String DB_URL = System.getProperty("DB_URL");
+  private final String DB_USER = System.getProperty("DB_USER");
+  private final String DB_PASSWORD = System.getProperty("DB_PASSWORD");
 
-  public DatabaseConnection(){
+  public DatabaseConnection() throws IllegalArgumentException {
+
+    if(DB_URL == null || DB_URL.isEmpty()) {
+      throw new IllegalArgumentException("Erro ao acessar a variável de ambiente DB_URL");
+    }
+
+    if(DB_USER == null || DB_USER.isEmpty()) {
+      throw new IllegalArgumentException("Erro ao acessar a variável de ambiente DB_USER");
+    }
+
+    if(DB_PASSWORD == null || DB_PASSWORD.isEmpty()) {
+      throw new IllegalArgumentException("Erro ao acessar a variável de ambiente DB_PASSWORD");
+    }
+
     HikariDataSource ds = new HikariDataSource();
-
-    ds.setJdbcUrl(System.getProperty("DB_URL"));
-    ds.setUsername(System.getProperty("DB_USER"));
-    ds.setPassword(System.getProperty("DB_PASSWORD"));
-
+    ds.setJdbcUrl(DB_URL);
+    ds.setUsername(DB_USER);
+    ds.setPassword(DB_PASSWORD);
     this.connectionDataSource = ds;
   }
 
