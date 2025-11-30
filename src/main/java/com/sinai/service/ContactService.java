96bb4fc9c15edab77ae9edc4d2af56a95088322d
ymillerr <millerandvitor@gmail.com>
2025-11-30 16:57:14 +0000

    feat: create properties validations for ContactService

--- src/main/java/com/sinai/service/ContactService.java
+++ src/main/java/com/sinai/service/ContactService.java
@@ -1,10 +1,14 @@
 package com.sinai.service;
 
+import java.util.function.Predicate;
+
 import javax.sql.DataSource;
 
 import com.sinai.exception.ContactException;
 import com.sinai.model.dao.ContactDAO;
 import com.sinai.model.entity.Contact;
+import com.sinai.util.Format;
+import com.sinai.util.Validator;
 
 public class ContactService {
   private ContactDAO Contactrepository;
@@ -13,6 +17,15 @@ public class ContactService {
     this.Contactrepository = new ContactDAO(db);
   }
 
-  public void createContact(Contact contact) {
+  public void createContact(Contact contact) throws ContactException {
+    String phoneFormated = Format.PhoneFormat(contact.getPhone());
+
+    if(Validator.isBlankOrNull(phoneFormated) || phoneFormated.length() > 13) throw new ContactException("O número de telefone está em um formato incorreto!");
+
+    if(Validator.isBlankOrNull(contact.getEmail()) || !contact.getEmail().contains("@")) throw new ContactException("Email invlálido ou vazio!");
+
+    if(Validator.isBlankOrNull(contact.getFirstName())) throw new ContactException("Nome inválido ou vazio!");
+
+    if(Validator.isBlankOrNull(contact.getMessage())) throw new ContactException("Menságem inválida ou vazia!");
   }
 }
